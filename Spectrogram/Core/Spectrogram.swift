f5e7812038d5c9d5cd134331b999d5ab9c45205b
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-20 17:09:04 +0900

    Initial Commit

---
+++ Spectrogram/Core/Spectrogram.swift
@@ -0,0 +1,10 @@
+//
+//  Spectrogram.swift
+//  Spectrogram
+//
+//  Created by 김제필 on 7/18/22.
+//
+
+import Foundation
+import Accelerate
+import UIKit
---
+++ Spectrogram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spectrogram
+//
+//  Created by 김제필 on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
