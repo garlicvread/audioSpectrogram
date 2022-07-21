6046980f1a7bd055ee5584b8bbc5090d4f76f547
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-22 04:25:06 +0900

    주석 추가

--- Spectrogram/Core/Spectrogram.swift
+++ Spectrogram/Core/Spectrogram.swift
@@ -16,17 +16,21 @@ import UIKit
 // 관련 문서: https://developer.apple.com/documentation/quartzcore/calayer
 public class Spectrogram: CALayer {
 
+    // 생성자.
+    // override init(): 상속받은 생성자를 호출하는 생성자.
     override init() {
         super.init()
 
+        // contentsGravity: 콘텐츠의 중심을 결정함.
         contentsGravity = .resize
     }
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    // required: 이 생성자가 반드시 구현되어야 한다는 제약조건
+    required init?(coder: NSCoder) {  // ?: 생성자가 없을 수도 있음.
+        fatalError("init(coder:) has not been implemented")  // 생성자가 없을 경우 오류를 발생시킴.
     }
 
-    override public init(layer: Any) {
+    override public init(layer: Any) {  // layer: 부모 레이어의 인스턴스. Any: 모든 타입의 인스턴스.
         super.init(layer: layer)
     }
 }
