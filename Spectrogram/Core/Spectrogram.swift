978f50b9c93f0704cc1da233c00aa5cf77908faf
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-22 12:06:08 +0900

    The annotation has been modified.

--- Spectrogram/Core/Spectrogram.swift
+++ Spectrogram/Core/Spectrogram.swift
@@ -21,11 +21,15 @@ public class Spectrogram: CALayer {
     override init() {
         super.init()
 
-        // contentsGravity: 콘텐츠의 중심을 결정함.
+        // contentsGravity: 레이어 내 컨텐츠의 배율 결정
+        // 컨텐츠의 실제 사이즈가 (w, h) 라면 화면에 표시되는 사이즈는 (w / contentsScale, h / contentsScale)
+        // .resize: 컨텐츠가 전체 레이어 크기에 맞게 리사이즈되도록 지정하는 옵션
         contentsGravity = .resize
     }
 
     // required: 이 생성자가 반드시 구현되어야 한다는 제약조건
+    // NSCoder: NextStep Coder. 과거 넥스트 사의 유물... 서브클래스가 오브젝트나 어떤 값을 메모리 - 다른 포맷 간에 변환할 수 있도록 지원함.
+    // archiving (오브젝트나 데이터의 저장) 및 distribution (오브젝트나 데이터의 프로세스 또는 스레드 간 복사) 등의 작업을 위해 사용됨.
     required init?(coder: NSCoder) {  // ?: 생성자가 없을 수도 있음.
         fatalError("init(coder:) has not been implemented")  // 생성자가 없을 경우 오류를 발생시킴.
     }
